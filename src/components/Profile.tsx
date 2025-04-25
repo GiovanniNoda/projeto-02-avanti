290117642ca8cba824a42039bd5a1d14928c7eaf
Giovanni <giovanniminoru@gmail.com>
2025-04-25 03:18:24 -0300

    responsive finished

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,15 +6,15 @@ interface ProfileInfoProps {
 
 export function Profile({ userData }: ProfileInfoProps) {
     return (
-        <div className="w-[80%] flex flex-col items-center gap-8 bg-bg-gray rounded-3xl py-6 px-10 lg:w-[90%] lg:flex-row xl:w-[80%]">
-            <div className="w-[232px] h-[232px] rounded-[50%] overflow-hidden bg-bg-gray border-3 border-blue-700 lg:w-[176px] lg:h-[176px] xl:w-[232px] xl:h-[232px]">
+        <div className="w-[95%] flex flex-col items-center gap-8 bg-bg-gray rounded-3xl py-6 px-10 md:w-[80%] lg:w-[90%] lg:flex-row xl:w-[80%]">
+            <div className="w-[128px] h-[128px] rounded-[50%] overflow-hidden bg-bg-gray border-3 border-blue-700 md:w-[176px] md:h-[176px] xl:w-[232px] xl:h-[232px]">
                 <img src={userData.avatar_url} alt="Foto de perfil do usuário" className="w-full h-full object-cover block" />
             </div>
 
-            <div className="w-[70%] flex flex-col items-center gap-6 lg:items-start">
-                <h2 className="text-blue-700 font-medium text-xl">{userData.name}</h2>
+            <div className="w-[90%] flex flex-col items-center gap-6 lg:items-start md:w-[70%]">
+                <h2 className="text-lg text-blue-700 font-medium md:text-xl">{userData.name}</h2>
 
-                <p>{userData.bio}</p>
+                <p className="text-sm text-primary md:text-[1rem]">{userData.bio}</p>
             </div>
         </div>
     )
